09a4c5b34513589d2a4ff3be1169b10b8c264dd9
Matt-Lamo <mattlejeune.lamogau@gmail.com>
2025-10-25 12:51:52 -0400

    Completed Assignment

--- PeopleDashboardTemplate.js
+++ PeopleDashboardTemplate.js
@@ -15,10 +15,39 @@ async function getPeople() {
 async function buildTable() {
 	try {
 		const data = await getPeople();
+		const peopleArray = data?.results || [];
+
+		for(let i = 0; i < peopleArray.length; i++) {
+			const person = peopleArray[i];
+			const fullName = `${person.name.title} ${person.name.first} ${person.name.last}`;
+			const address = `${person.location.street.number} ${person.location.street.name}`;
+			const city = `${person.location.city}`;
+			const state = `${person.location.state}`;
+			const zipcode = `${person.location.postcode}`;
+			const latitude = `${person.location.coordinates.latitude}`;
+			const longitude = `${person.location.coordinates.longitude}`;
+			const phone = `${person.phone}`;
+			const newRow = `<tr id="${i}"><td>${fullName}</td><td>${address}</td><td>${city}</td><td>${state}</td><td>${zipcode}</td><td>${latitude}</td><td>${longitude}</td></tr>`;
+			$('#people').append(newRow);
+			
+			const rowEl = document.getElementById(i.toString());
+			rowEl.addEventListener('mouseover', function() {
+				this.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+				this.title = phone;
+			});
+			rowEl.addEventListener('mouseout', function() {
+				this.style.backgroundColor = '';
+			});
+			
+		}
+		console.log(peopleArray);
 	} catch (e) {
 		console.log("Error " + e);
 	}
 }
 
 buildTable();
+
+
+
 
